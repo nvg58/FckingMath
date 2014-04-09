376d792d1867aefe31b33ea82c69a7f98d43537b
Giap Nguyen <nvangiap5894@gmail.com>
2014-04-09 20:20:33 +0700

    first ver

---
+++ src/StartScene.js
@@ -0,0 +1,59 @@
+var StartLayer = cc.LayerColor.extend({
+  screenSize: null,
+  centerPos: null,
+  ctor: function () {
+    this._super();
+  },
+  init: function () {
+    this._super();
+    this.setColor(cc.c3b(44, 62, 80));
+    screenSize = cc.Director.getInstance().getWinSize();
+    centerPos = cc.p(screenSize.width / 2, screenSize.height / 2);
+
+    var logo = cc.LabelTTF.create("FCKING MATH", "Helvetica", 70);
+    logo.setColor(cc.WHITE);
+    logo.setPosition(cc.p(screenSize.width/2,  screenSize.height - 200));
+    this.addChild(logo);
+
+    var startBtn = cc.MenuItemSprite.create(
+      cc.Sprite.create(s_start_n),
+      cc.Sprite.create(s_start_s),
+      this.onPlay, this);
+    var rate = cc.MenuItemSprite.create(
+      cc.Sprite.create(s_rate_n),
+      cc.Sprite.create(s_rate_s),
+      this.onRate, this);
+    var leaderboard = cc.MenuItemSprite.create(
+      cc.Sprite.create(s_leaderboard_n),
+      cc.Sprite.create(s_leaderboard_s),
+      this.onShowLB, this);
+
+    var offsetY = startBtn.getContentSize().height + 20;
+    var offsetX = startBtn.getContentSize().width / 2 + 40;
+    startBtn.setPosition(centerPos);
+    rate.setPosition(cc.p(screenSize.width / 2 - offsetX, screenSize.height / 2 - offsetY));
+    leaderboard.setPosition(cc.p(screenSize.width / 2 + offsetX, screenSize.height / 2 - offsetY));
+    var menu = cc.Menu.create(startBtn, rate, leaderboard);
+    menu.setPosition(cc.p(0, 0));
+    this.addChild(menu);
+  },
+  onPlay: function () {
+    cc.log("start clicked");
+    cc.Director.getInstance().replaceScene(cc.TransitionMoveInT.create(0.2, new PlayScene()));
+  },
+  onRate: function () {
+
+  },
+  onShowLB: function () {
+
+  }
+});
+
+var StartScene = cc.Scene.extend({
+  onEnter: function () {
+    this._super();
+    var layer = new StartLayer();
+    layer.init();
+    this.addChild(layer);
+  }
+});
---
+++ src/resource.js
@@ -0,0 +1,65 @@
+var s_HelloWorld = "HelloWorld.jpg";
+var s_CloseNormal = "CloseNormal.png";
+var s_CloseSelected = "CloseSelected.png";
+var s_start_n = "start_n.png";
+var s_start_s = "start_s.png";
+var s_rate_n = "rate_n.png";
+var s_rate_s = "rate_s.png";
+var s_leaderboard_n = "leaderboard_n.png";
+var s_leaderboard_s = "leaderboard_S.png";
+var s_restart_n = "restart_n.png";
+var s_restart_s = "restart_s.png";
+var s_to_start_n = "to_start_n.png";
+var s_to_start_s = "to_start_s.png";
+var s_gameover = "gameover.png";
+var s_right_n = "right_n.png";
+var s_right_s = "right_s.png";
+var s_wrong_n = "wrong_n.png";
+var s_wrong_s = "wrong_s.png";
+var score = 0;
+var f_lato = "Arial";
+var s_loading = "loading.png";
+var TG_TIMER = 1;
+var e_passed = "res/passed.mp3";
+var e_fail = "res/fail.mp3";
+var e_reload = "res/reload.mp3";
+var TAG_PLAY_SCENE = 10;
+var TAG_GAME_OVER = 11;
+var TAG_START_SCENE = 12;
+
+var g_resources = [
+  //image
+  {src: s_HelloWorld},
+  {src: s_CloseNormal},
+  {src: s_CloseSelected},
+  {src: s_start_n},
+  {src: s_start_s},
+  {src: s_rate_n},
+  {src: s_rate_s},
+  {src: s_leaderboard_n},
+  {src: s_leaderboard_s},
+  {src: s_restart_n},
+  {src: s_restart_s},
+  {src: s_to_start_s},
+  {src: s_to_start_n},
+  {src: s_gameover},
+  {src: s_right_s},
+  {src: s_right_n},
+  {src: s_wrong_s},
+  {src: s_wrong_n},
+  {src: s_loading},
+
+  //plist
+
+  //fnt
+//  {src: f_lato}
+  //tmx
+
+  //bgm
+
+  //effect
+  {src: e_passed},
+  {src: e_fail},
+  {src: e_reload}
+
+];
