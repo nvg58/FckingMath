fd070e4bdef251bf022f38b7d07508ca25b4145b
Giap Nguyen <nvangiap5894@gmail.com>
2014-04-11 17:09:43 +0700

    can usr keyboard space

--- main.js
+++ main.js
@@ -70,10 +70,10 @@ var cocos2dApp = cc.Application.extend({
         cc.EGLView.getInstance().resizeWithBrowserSize(true);
 
         // turn on display FPS
-        director.setDisplayStats(this.config['showFPS']);
+//        director.setDisplayStats(this.config['showFPS']);
 
         // set FPS. the default value is 1.0/60 if you don't call this
-        director.setAnimationInterval(1.0 / this.config['frameRate']);
+//        director.setAnimationInterval(1.0 / this.config['frameRate']);
 
         //load resources
         cc.AudioEngine.getInstance().preloadEffect(e_fail);
